c9dc9f8bb163f049277b4270893adbfd56793313
Wenceslau Neto <wbaneto@gmail.com>
2024-05-19 22:43:03 +0100

    Add ELK stack and logging setup to Docker configurations
    The commit introduces ELK stack (ElasticSearch, Logstash, and Kibana) to the existing Docker configurations. It also includes the addition of a Filebeat service to the app's Docker Compose file for better container logging. Moreover, the logging level has been set to 'INFO' in various application environment configurations.

--- sandbox/down.sh
+++ sandbox/down.sh
@@ -4,8 +4,10 @@ sleep 5
 
 echo "finalizando services..."
 docker compose -f services/docker-compose.yml down
+docker compose -f elk/docker-compose.yml down
 sleep 5
 
 echo "Removendo network..."
 docker network rm adm_videos_services
+docker network rm elastic
 sleep 1
--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,18 +1,23 @@
 # Criar as docker networks
 docker network create adm_videos_services
+docker network create elastic
 echo "Criando network..."
 sleep 1
 
 # Criar as pastas com permissões
+#sudo chown root app/filebeat/filebeat.docker.yml
 #mkdir -m 777 .docker
+#mkdir -m 777 .docker/es01
 #mkdir -m 777 .docker/keycloak
+#mkdir -m 777 .docker/filebeat
 
 docker compose -f services/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
 echo "Inicializando os containers..."
 sleep 20
 
 docker compose -f app/docker-compose.yml up -d
 #command to run and force the rebuild
 #docker compose -f app/docker-compose.yml up -d --build
-echo "Inicializando os aplicação..."
+echo "Inicializando aplicação..."
 sleep 1
