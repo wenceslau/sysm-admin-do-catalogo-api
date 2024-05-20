f8df5a8621ba100dff9d31128a6b8b59900ec6f1
Wenceslau Neto <wbaneto@gmail.com>
2024-05-20 22:57:42 +0100

    Update logstash.conf and Main.java with new functionalities
    In the logstash.conf file, a ruby filter has been added to retrieve kv_tags from the incoming message and a kv filter has been added to handle the kv_tags. Additionally, logging statements have been introduced in the Main.java file for tracking the Spring initialization process.

--- infrastructure/src/main/java/com/sysm/catalog/admin/infrastructure/Main.java
+++ infrastructure/src/main/java/com/sysm/catalog/admin/infrastructure/Main.java
@@ -7,6 +7,8 @@ import com.sysm.catalog.admin.infrastructure.aggregates.category.persistence.Cat
 import com.sysm.catalog.admin.infrastructure.aggregates.genre.persistence.GenreRepository;
 import com.sysm.catalog.admin.infrastructure.aggregates.video.persistence.VideoRepository;
 import com.sysm.catalog.admin.infrastructure.configuration.WebServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,10 +19,14 @@ import org.springframework.core.env.AbstractEnvironment;
 @SpringBootApplication
 public class Main {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOG.info("[step:to-be-init] [id:1] Inicializando o Spring");
         // Default profile
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
+        LOG.info("[step:inittialized] [id:2] Spring inicializado..");
     }
 
 //    @RabbitListener(queues = "video.encoded.queue")
